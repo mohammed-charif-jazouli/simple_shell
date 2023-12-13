8c597664073056efa66122009a7e9ed40ffb6c2a
charif <mohammedcharifj@gmail.com>
2023-12-13 22:49:45 +0100

    updated

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-readme file
+0x16. C - Simple Shell
--- shell.h
+++ shell.h
@@ -1,48 +1,36 @@
-#ifndef _SHELL_H_
-#define _SHELL_H_
-
+#ifndef SHELL_H
+#define SHELL_H
 #include <stdio.h>
+#include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
-#include <fcntl.h>
-#include <sys/types.h>
-#include <sys/stat.h>
 #include <sys/wait.h>
-#include <unistd.h>
-#include <errno.h>
-
-#define SPECIF " \t\r\a\n"
+#include <signal.h>
+#include <stdbool.h>
+#define MAX_C 10
+/**
+ * struct denum - structure that contains vars
+ * @cnt: lines cnt
+ */
+typedef struct denum
+{
+	int cnt;
+} denum;
+void prompt(char **arv, char **envp, bool flg);
+int _strcmp(char *s1, char *s2);
+char *_strcat(char *dest, char *src);
+char *handle_path(char **rgv, char *cmd);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
+void handle_exit(char *cmd);
+void runcmd(char **rgv, char **arv, char **envp);
+char *trim(char *cmd);
+char *_strncpy(char *dest, char *src, int n);
+char *get_path(char *cmd);
+char *_getenv(char *name);
+char **tokenize_env(char *path);
 extern char **environ;
-
-/* Built-in functions */
-void exit_shell(char **cmd, char **argv, int *status, int indx);
-void print_environment(char **cmd, int *status);
-int change_directory(const char *target_dir);
-
-/* Tool functions */
-char *read_line(void);
-int shell_loop(char **argv);
-char **tokenize(char *input);
-char *get_full_path(char *cmd);
-char *get_environment_variable(char *var);
-int execute_command(char **cmd, char **argv, int count, int indx);
-void print_error(char *argv_0, char *cmd, int indx);
-int is_builtin_command(char *cmd);
-void handle_builtin_commands(char **cmd, char **argv, int *status, int indx);
-void exit_error(char **cmd, char **argv, int indx);
-
-/* String handler functions */
-char *_strdup(const char *s);
-int _strlen(const char *s);
-char *_strcpy(char *dest, const char *src);
-char *_strcat(char *dest, const char *src);
-int _strcmp(const char *s1, const char *s2);
-char *int_to_ascii(int n);
-void reverse_string(char *str, int len);
-int is_positive_number(const char *s);
-int _atoi(const char *s);
-
-/* Memory functions */
-void free_array(char **ary);
-
-#endif /* _SHELL_H_ */
+/*void geterror(int cnt, char **arv, char *cmd);*/
+void geterror(denum *n, char **arv, char *cmd);
+/*void search_path(char **rgv, char *cmd, char **envp);*/
+#endif
